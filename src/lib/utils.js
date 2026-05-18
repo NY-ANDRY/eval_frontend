9e986eefee672582f8e1fea212b4d52ba2a5b997
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2026-05-18 15:25:05 +0300

    date format

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -22,8 +22,12 @@ export const formatDate = (str) => {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
   });
-  return result.replace(/\//g, "-");
+  // result = result.replace(/\//g, "-");
+  return result;
 };
 
 export const datePicktoDateTime = (datePickerValue) => {
